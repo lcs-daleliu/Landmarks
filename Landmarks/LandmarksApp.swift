3d02fbd66c946eaef1602fcf5e32f74302ee0a97
Dale Liu <YLiu@lcs.on.ca>
2024-01-27 10:05:25 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yiren LIU on 2024-01-27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
